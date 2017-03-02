111fbd7354372fd34c6c05ffbb78a9c432c50cf8
matt12384 <matt12384@live.co.uk>
2017-03-02 15:43:44 +0000

    Add files via upload

---
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -0,0 +1,20 @@
+﻿using ProblemLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 Console.WriteLine(ProblemsOneToTen.SumOfMultiplesOf3And5BelowArgument(1000));
+            //2 Console.WriteLine(ProblemsOneToTen.GetSumOfEvenFibonacciNumbersBelowGivenArgument(4000000));
+            //3 Console.WriteLine(ProblemsOneToTen.LargestPrimeFactor(600851475143));
+            //10 Console.WriteLine(ProblemsOneToTen.SumOfPrimesBelowValueProvided(2000000));
+        }
+    }
+}
