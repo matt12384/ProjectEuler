a64351a5fd17d8c0a7c69c22ee1c36b3969efaa3
matthew.flynn <matthew.flynn@fdmgroup.com>
2017-03-31 09:39:45 +0100

    Refactoring

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -11,10 +11,25 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
+            #region 1 - 10
             //1 Console.WriteLine(ProblemsOneToTen.SumOfMultiplesOf3And5BelowArgument(1000));
             //2 Console.WriteLine(ProblemsOneToTen.GetSumOfEvenFibonacciNumbersBelowGivenArgument(4000000));
             //3 Console.WriteLine(ProblemsOneToTen.LargestPrimeFactor(600851475143));
+            //4 Console.WriteLine(ProblemsOneToTen.LargestPalindromeProductOfTwoThreeDigitNumbers());
+            //5 Console.WriteLine(ProblemsOneToTen.SmallestMultiple());
+            //6 Console.WriteLine(ProblemsOneToTen.SumSquareDifference());
+            //7 Console.WriteLine(ProblemsOneToTen.GetPrimeByIndex(10001));
+            //8 Console.WriteLine(ProblemsOneToTen.GetLargestProduct());
+            //9 Console.WriteLine(ProblemsOneToTen.GetProductOfPythagoreanTriplet(1000));
             //10 Console.WriteLine(ProblemsOneToTen.SumOfPrimesBelowValueProvided(2000000));
+            #endregion
+
+            #region 11 - 20
+            //11
+            //12 Console.WriteLine(ProblemsElevenToTwenty.HighlyDivisibleTriangularNumber(500));
+            //13
+            //14 Console.WriteLine(ProblemsElevenToTwenty.LongestCollatzSequence(1000000));
+            #endregion
         }
     }
 }
